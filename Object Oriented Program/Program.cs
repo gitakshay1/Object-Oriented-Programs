aa70a9316440b35c88e723647c37df758fcbd6bc
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-16 12:59:14 +0530

    [Akshay] Added program for json Invebtory data mangement

---
+++ Object Oriented Program/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using Object_Oriented_Program;
+
+JsonInventoryMain Json = new JsonInventoryMain();
+Json.Convert("C:\\Users\\aksha\\Assignments\\Object-Oriented-Programs\\Object Oriented Program\\Inventory.json");
